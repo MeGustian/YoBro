d450fbfd19c0456f107879ebe126727bfc3770f7
Megustian <yoro187@gmail.com>
2016-05-07 01:25:59 +0300

    continuingg.........

--- routes/content.js
+++ routes/content.js
@@ -14,6 +14,16 @@ router.get('/all', function(req, res, next) {
 
 module.exports = router;
 
-router.get('/decision', function(req, res, next){
-    var content = ContentSuggestions.find
-})
+router.get('/decision', function(req, res, next) {
+    var content = [];
+    var duration = req.params.duration;
+    var contentVibe = req.params.contentVibe;
+    ContentSuggestions.find({'contentVibe' : contentVibe}).where(duration).ls(duration).gt(duration * 0.8).exec(function (err, data) {
+        if (err) console.log('error finding content');
+        else content = data;
+
+        res.render('some_content', { contentSuggestions: content });
+    });
+
+
+});
