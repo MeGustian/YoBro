34c2a3a845bc42d0df2bc6255dcc4bcbc8542e76
Megustian <yoro187@gmail.com>
2016-05-06 15:38:36 +0300

    working user login logic

--- app.js
+++ app.js
@@ -1,14 +1,80 @@
+var fs = require('fs');
+var stylus = require('stylus');
+var nib = require('nib');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
+var session = require('express-session');
+
+// Database
+var mongo = require('mongodb');
+var monk = require('monk');
+var mongoose = require('mongoose');
+var db = monk('yobro:yobropwd@ds015892.mlab.com:15892/yobro');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// ==============CONFIGURARTIONS=============================
+
+mongoose.connect('yobro:yobropwd@ds015892.mlab.com:15892/yobro');
+require('./config/passport')(passport); // pass passport for configuration
+//require('./routes/index.js')(passport);
+/* UNCOMMENT THIS AFTER ADDING FAVICON IN Public FOLDER */
+//app.use(favicon(__dirname + '/public/favicon.ico')); // Favicon
+
+function compile(str, path) {
+  return stylus(str)
+      .set('filename', path)
+      .use(nib())
+      .import('nib');
+}
+// Set views with Jade. Read below to understand the __dirname thing.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+// Using Stylus with nib.
+app.use(stylus.middleware(
+    { src: __dirname + '/public'
+      , compile: compile
+    }
+));
+
+// Set `/public` as the static resources, which is accessible from the browser
+// as if public was the root directory.
+// Example: If I want to access `/public/images/logo.png`, I could write
+// `__full-site-URL__/images/logo.png`.
+app.use(express.static(__dirname + '/public'));
+// TODO: Understand this.
+app.use(bodyParser.json());
+// TODO: Understand this.
+app.use(bodyParser.urlencoded({extended: true}));
+// TODO: Understand this.
+// app.use(app.router);
+app.use(cookieParser()); // read cookies (needed for auth)
+// required for passport
+app.use(session({ secret: 'thisisasecrettest' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// Connect to DB
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+
+});
+
+
+app.use('/', routes);
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash');
+var passport = require('passport');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/login', function(req, res) {
+
+  // render the page and pass in any flash data if it exists
+  //res.render('login', { message: req.flash('loginMessage') });
+  res.render('login');
+});
+
+router.post('/login', passport.authenticate('local-login', {
+  successRedirect : '/', // redirect to the secure profile section
+  failureRedirect : '/users/login', // redirect back to the signup page if there is an error
+  failureFlash : true // allow flash messages
+}))
+console.log("HERHEHRHE");;
+
+/* GET Signup page */
+router.get('/signup', function(req, res) {
+
+  // render the page and pass in any flash data if it exists
+  res.render('signup', { message: req.flash('loginMessage') });
+  //res.render('signup');
+});
+
+/* POST Signup credentials */
+router.post('/signup', passport.authenticate('local-signup', {
+  successRedirect : '/', // redirect to the secure profile section
+  failureRedirect : '/users/signup', // redirect back to the signup page if there is an error
+  failureFlash : true // allow flash messages
+}));
 
 module.exports = router;
